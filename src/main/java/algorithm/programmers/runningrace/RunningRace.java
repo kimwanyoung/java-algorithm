878c0428633fd5ce71c680bcb0d5196f384cafec
kimwanyoung <dhks2869@gmail.com>
2023-09-03 20:47:07 +0900

    feat : 프로그래머스 달리기 경주 리팩터링

--- src/main/java/algorithm/programmers/runningrace/RunningRace.java
+++ src/main/java/algorithm/programmers/runningrace/RunningRace.java
@@ -9,25 +9,28 @@ public class RunningRace {
 		HashMap<Integer, String> basedOnRank = new HashMap<>();
 
 		int rank = 1;
-		for(String player: players) {
+		for (String player : players) {
 			basedOnRank.put(rank, player);
 			basedOnName.put(player, rank);
 
 			rank++;
 		}
 
-		for(String call: callings) {
+		for (String call : callings) {
 			int callingRank = basedOnName.get(call);
 			String frontPlayer = basedOnRank.get(callingRank - 1);
 
-			basedOnName.replace(call, callingRank - 1);
-			basedOnRank.replace(callingRank - 1, call);
-
-			basedOnName.replace(frontPlayer, callingRank);
-			basedOnRank.replace(callingRank, frontPlayer);
+			mapSwap(basedOnName, basedOnRank, call, callingRank - 1);
+			mapSwap(basedOnName, basedOnRank, frontPlayer, callingRank);
 		}
 
 		return basedOnRank.values().toArray(new String[0]);
+	}
+
+	public void mapSwap(HashMap<String, Integer> nameMap, HashMap<Integer, String> rankMap, String call,
+		int callingRank) {
+		nameMap.replace(call, callingRank);
+		rankMap.replace(callingRank, call);
 	}
 
 }
