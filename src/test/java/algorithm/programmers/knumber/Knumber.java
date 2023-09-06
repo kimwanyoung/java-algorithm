a182bcac6e0428140f70905a698f9b5a6ac48b9b
kimwanyoung <dhks2869@gmail.com>
2023-09-06 20:50:19 +0900

    feat : 프로그래머스 k번째 수

---
+++ src/test/java/algorithm/programmers/knumber/Knumber.java
@@ -0,0 +1,32 @@
+package algorithm.programmers.knumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Knumber {
+    public int[] solution(int[] array, int[][] commands) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int[] command : commands) {
+            int startIndex = command[0] - 1;
+            int endIndex = command[1];
+            int findIndex = command[2];
+            int[] slicedArray = getSortedSliceOfArray(array, startIndex, endIndex);
+
+            answer.add(slicedArray[findIndex - 1]);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] getSortedSliceOfArray(int[] array, int startIndex, int endIndex) {
+        int[] arr = new int[endIndex - startIndex];
+
+        for(int i = 0 ; i < arr.length; i++) {
+            arr[i] = array[startIndex + i];
+        }
+
+        Arrays.sort(arr);
+        return arr;
+    }
+}
---
+++ src/test/java/algorithm/programmers/knumber/KnumberTest.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.knumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class KnumberTest {
+    Knumber k = new Knumber();
+    @Test
+    void solution() {
+        //given
+        int[] arr = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        int[] result = {5, 6, 3};
+
+        //when
+        int[] answer = k.solution(arr, commands);
+
+        //then
+        Assertions.assertThat(answer).isEqualTo(result);
+
+    }
+}
