eac5804078f90d2fea858d666aa54159b44cfb8b
kimwanyoung <dhks2869@gmail.com>
2024-02-15 18:56:30 +0900

    feat : 프로그래머스 피도로 lv.2

---
+++ src/main/java/algorithm/programmers/피로도/Solution.java
@@ -0,0 +1,27 @@
+package algorithm.programmers.피로도;
+
+public class Solution {
+
+    public int answer; // 최대 던전 수
+    public boolean[] visited; // 방문 여부
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+
+        dfs(0, k, dungeons);
+
+        return answer;
+    }
+
+    public void dfs(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true; // 방문 처리
+                dfs(depth + 1, k - dungeons[i][1], dungeons); // 재귀 호출
+                visited[i] = false; // 방문 초기화
+            }
+        }
+
+        answer = Math.max(answer, depth);
+    }
+}
